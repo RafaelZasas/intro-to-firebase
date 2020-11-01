26dad0c0497438ba049fa319ddd12daaef3e3e98
Rafael Zasas <zasas.rafi@gmail.com>
2020-10-31 17:19:18 -0700

    Add Firebase 🔥
    📦Added firebase-tools npm package
    ➕ Firebase App 🔥
    ➕Firebase Analytics  📈
    
    🙈Updated Gitignore :
     - Removed Idea Files
     - I Purposefully left out the firebase config files so that you cold both have them 👥 .
    
    🏗 Moved src code into /public dir

---
+++ public/src/js/app.js
@@ -0,0 +1,18 @@
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyCbhU8BmjoxnZ128tZyRUKc4U2p4Q0j9Iw",
+    authDomain: "ecommerce-with-firebase-a7af6.firebaseapp.com",
+    databaseURL: "https://ecommerce-with-firebase-a7af6.firebaseio.com",
+    projectId: "ecommerce-with-firebase-a7af6",
+    storageBucket: "ecommerce-with-firebase-a7af6.appspot.com",
+    messagingSenderId: "1086804382186",
+    appId: "1:1086804382186:web:c8ca6c306569919e132293",
+    measurementId: "G-7FEL1DERZ2"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+console.log(firebase)
