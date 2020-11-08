e4d1bfa34ea390672353c010395e72859fce1998
ANDREYDEN <andrey2850@gmail.com>
2020-11-07 21:28:14 -0500

    Restructured auth logic

--- public/src/js/auth.js
+++ public/src/js/auth.js
@@ -1,9 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const $logInButton = document.getElementById('log-in');
-    $logInButton.addEventListener('click', async () => {
+class Auth {
+    static isSignedIn() {
+        return !!firebase.auth().currentUser
+    }
+
+    static async signInWithGoogle() {
         const provider = new firebase.auth.GoogleAuthProvider();
 
-        // Raf- I added this as given by docs: https://firebase.google.com/docs/auth/web/auth-state-persistence
         try {
             await firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
             // Existing and future Auth states are now persisted in the current
@@ -18,24 +20,9 @@ document.addEventListener('DOMContentLoaded', () => {
             var errorMessage = error.message;
             console.log(`Error Code:${errorCode}\nError Message:${errorMessage}`)
         }
-    });
+    }
 
-    firebase.auth().onAuthStateChanged((user) => {
-        displayProfileUI(); // call method to update UI according to users log in state
-    })
-});
-
-/**
- * Checks if the user exists and changes the UI appropriately
- */
-function displayProfileUI() {
-    var user = firebase.auth().currentUser;
-
-    const userSignedIn = !!user
-    console.log(userSignedIn); // print true if user exists
-    // hide the login and sign up buttons when user signs in
-    document.getElementById('loginSection').hidden = userSignedIn;
-    // show the profile button when user signs in
-    document.getElementById('ProfileSection').hidden = !userSignedIn;
-    return userSignedIn;
+    static async signOut() {
+        await firebase.auth().signOut()
+    }
 }
