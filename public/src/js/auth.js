a0e00e5baad5eea18e3aea50b82b3e02467fc0f2
Rafael Zasas <zasas.rafi@gmail.com>
2020-11-07 18:17:02 -0800

    🎨💄 UI
    Updated header tags for each page.
    
    🚧 Added a resources page.  Still plan to add specific links under each subheading. 
    
    ⚡️ Also refactored the login/ logout functionality to use onClick rather than doc event listeners

--- public/src/js/auth.js
+++ public/src/js/auth.js
@@ -1,34 +1,59 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const $logInButton = document.getElementById('log-in');
-    $logInButton.addEventListener('click', async () => {
-        const provider = new firebase.auth.GoogleAuthProvider();
 
-        // Raf- I added this as given by docs: https://firebase.google.com/docs/auth/web/auth-state-persistence
-        try {
-            await firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
-            // Existing and future Auth states are now persisted in the current
-            // session only. Closing the window would clear any existing state even
-            // if a user forgets to sign out.
-            // ...
-            // New sign-in will be persisted with session persistence.
-            const auth_result = await firebase.auth().signInWithPopup(provider);
-        } catch (error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            console.log(`Error Code:${errorCode}\nError Message:${errorMessage}`)
-        }
+async function login() {
+    
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    // added this as given by docs: https://firebase.google.com/docs/auth/web/auth-state-persistence
+    try {
+        await firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+        // Existing and future Auth states are now persisted in the current
+        // session only. Closing the window would clear any existing state even
+        // if a user forgets to sign out.
+        // ...
+        // New sign-in will be persisted with session persistence.
+        const auth_result = await firebase.auth().signInWithPopup(provider);
+    } catch (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(`Error Code:${errorCode}\nError Message:${errorMessage}`)
+    }
+
+    updateUI();
+    
+    
+}
+
+async function logout() {
+
+    // event handler for logout button
+
+    await firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+    firebase.auth().signOut().then(function () {
+        // Sign-out successful.
+        console.log('User signed out successfully.')
+    }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(`Error Code:${errorCode}\nError Message:${errorMessage}`)
     });
 
+    updateUI();
+
+}
+
+function updateUI(){
     firebase.auth().onAuthStateChanged((user) => {
-        displayProfileUI(); // call method to update UI according to users log in state
+        displayAuthenticatedUI(); // call method to update UI according to users log in state
     })
-});
+}
+
 
 /**
  * Checks if the user exists and changes the UI appropriately
  */
-function displayProfileUI() {
+function displayAuthenticatedUI() {
     var user = firebase.auth().currentUser;
 
     const userSignedIn = !!user
@@ -39,3 +64,5 @@ function displayProfileUI() {
     document.getElementById('ProfileSection').hidden = !userSignedIn;
     return userSignedIn;
 }
+
+
