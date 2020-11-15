a1881a62209407cb3803f6bea3a10cc0c2c0379e
Rafael Zasas <zasas.rafi@gmail.com>
2020-11-14 18:45:21 -0800

    🎨⚡️ Dynamically populate Navbar
    Add a function populateNavbar() in main.js
    
    pages now call populateNavbar() on <body> load.
    
    Index.html needs to be updated manually to avoid path issues.

---
+++ public/src/js/main.js
@@ -0,0 +1,102 @@
+// This function will be called in every html page other than index due to path's being different
+function populateNavbar(){
+    let headTag = document.querySelector('#navbar');
+    headTag.innerHTML = `
+    <div class="navbar-brand">
+        <a class="navbar-item" href="../../index.html">
+            <img src="../assets/dsc_lockup.png" width="112" height="28">
+        </a>
+
+        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false"
+           data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+        </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+
+            <div class="navbar-item has-dropdown is-hoverable">
+
+                <a class="navbar-link">
+                    Products
+                </a>
+
+                <!--   dropdown section for products -->
+                <div class="navbar-dropdown">
+                    <a class="navbar-item" href="../html/shoes.html">
+                        Shoes
+                    </a>
+                    <a class="navbar-item" href="../html/shirts.html">
+                        Shirts
+                    </a>
+                    <a class="navbar-item" href="../html/bags.html">
+                        Bags
+                    </a>
+                    <a class="navbar-item" href="../html/hats.html">
+                        Hats
+                    </a>
+                </div>
+            </div>
+            
+            <a class="navbar-item" href="https://dsc.community.dev"> <!--  could be cool to implement google maps api -->
+                Locations
+            </a>
+
+
+            <!--  dropdown section for More -->
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">
+                    More
+                </a>
+
+                <div class="navbar-dropdown">
+                    <a class="navbar-item" href="../html/resources.html">
+                        Resources
+                    </a>
+                    <a class="navbar-item" href="https://github.com/RafaelZasas/intro-to-firebase/blob/develop
+/docs/CONTRIBUTING.md">
+                        Contribute to this website
+                    </a>
+                    <hr class="navbar-divider">
+                    <a class="navbar-item" href="https://github.com/RafaelZasas/intro-to-firebase/issues">
+                        Report an issue
+                    </a>
+                </div>
+            </div>
+
+
+        </div>
+        <div id='loginSection'>
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <a class="button is-primary" id="sign-up">
+                            <strong>Sign up</strong>
+                        </a>
+                        <a class="button is-light" onclick="login()" id="log-in">
+                            <strong>Log in</strong>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div hidden id='ProfileSection'>
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <a class="button is-primary" href="../html/profile.html">
+                            <strong>Profile</strong>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    `;
+}
