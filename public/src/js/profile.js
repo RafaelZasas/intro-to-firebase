8f8f151d1f33a733259bd66b13bc90f7ec689373
ANDREYDEN <andrey2850@gmail.com>
2020-11-18 20:59:53 -0500

    Created an email verification display

--- public/src/js/profile.js
+++ public/src/js/profile.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     firebase.auth().onAuthStateChanged(populateProfile)
-    populateProfile()
 })
 
 function populateProfile(user) {
     if (user) {
         document.getElementById('fullName').innerHTML = user.displayName;
         document.getElementById('small_profile_image').setAttribute('src', user.photoURL);
+        document.getElementById('email-verified-success').style.display = user.emailVerified ? "inline-flex" : "none"
+        document.getElementById('email-verified-failure').style.display = user.emailVerified ? "none" : "inline-flex"
     }
 }
