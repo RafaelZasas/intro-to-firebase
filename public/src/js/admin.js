0d493f2b61f240ddbfb740326219a9bef74ce00b
ANDREYDEN <andrey2850@gmail.com>
2020-12-08 19:34:40 -0500

    Added users list

--- public/src/js/admin.js
+++ public/src/js/admin.js
@@ -1,7 +1,21 @@
 firebase.auth().onAuthStateChanged(getUsers)
 
-async function getUsers(user) {
+async function getUsers(currentUser) {
+    const $usersSection = document.getElementById('usersSection')
+    if (await isAdmin(currentUser)) {
+
+        const usersCollection = await firebase.firestore().collection('users').get()
+        const emails = usersCollection.docs.map(userDoc => userDoc.data().email)
+
+        $usersSection.innerText = `${emails.join(' | ')}`
+    } else {
+        $usersSection.innerText = 'Insufficient permissions'
+    }
+}
+
+async function isAdmin(user) {
+    if (!user) return false;
     const userDocReference = await firebase.firestore().doc(`users/${user.uid}`).get()
-    const isAdmin = userDocReference.data().permissions.admin
-    console.log(`This user is ${isAdmin ? '' : 'not '}an admin`);
+    if (!userDocReference.exists) return false
+    return userDocReference.data().permissions.admin
 }
