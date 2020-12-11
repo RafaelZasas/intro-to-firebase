aeba68961e2c5ccdf99284995d0069019059d373
ANDREYDEN <andrey2850@gmail.com>
2020-12-10 21:38:23 -0500

    Made user rows prettier in the admin panel

--- public/src/js/admin.js
+++ public/src/js/admin.js
@@ -2,15 +2,34 @@ firebase.auth().onAuthStateChanged(getUsers)
 
 async function getUsers(currentUser) {
     const $usersSection = document.getElementById('usersSection')
+    const $usersRows = document.getElementById('usersRows')
     if (await isAdmin(currentUser)) {
 
         const usersCollection = await firebase.firestore().collection('users').get()
-        const emails = usersCollection.docs.map(userDoc => userDoc.data().email)
+        const userRows = usersCollection.docs
+            .map(userDoc => getUserRow(userDoc.data()))
 
-        $usersSection.innerText = `${emails.join(' | ')}`
+        $usersRows.innerHTML = userRows.join('\n')
     } else {
         $usersSection.innerText = 'Insufficient permissions'
     }
+}
+
+function getUserRow(user) {
+    return `
+    <tr>
+        <td>${user.uid}</td>
+        <td>${user.email}</td>
+        <td>
+            <div class="is-primary select">
+                <select>
+                    <option value="user" ${user.permissions.admin ? '' : 'selected'}>Mortal User</option>
+                    <option value="admin" ${user.permissions.admin ? 'selected' : ''}>Almighty Admin</option>
+                </select>
+            </div>
+        </td>
+    </tr>
+    `
 }
 
 async function isAdmin(user) {
