7b26303965c14e4ec62c001316be3718439d79a3
ANDREYDEN <andrey2850@gmail.com>
2021-01-11 18:29:20 -0500

    Refactored .then into async/await

--- public/src/js/firestore.js
+++ public/src/js/firestore.js
@@ -3,43 +3,43 @@
 This section deals with firestore calls related to product data
  */
 let productsSection = document.querySelector(`#productsSection`);
-function getProducts(product_type) {
+async function getProducts(product_type) {
     // this function queries the firestore database for all the product docs within the selected category
 
     sessionStorage.setItem("productType", product_type) // set globally for use in getProductInfo()
 
     let index = 0; // to keep track of which product document were passing through
 
-    db.collection(`products/${product_type}/inventory`).get().then((querySnapshot) => {
+    const querySnapshot = await db.collection(`products/${product_type}/inventory`).get()
 
-        querySnapshot.docs.forEach((doc) => {
-
-            populateProductCards(doc, index);// add each card individually; index is used to reference each of the cards
-            index++;
-        });
-
-        productsSection.innerHTML += productsHTML; // adds all of the cards html to the products grid
-
+    querySnapshot.docs.forEach((doc) => {
+        populateProductCards(doc, index);// add each card individually; index is used to reference each of the cards
+        index++;
     });
+
+    productsSection.innerHTML += productsHTML; // adds all of the cards html to the products grid
 }
 
-function getProductInfo() {
+async function getProductInfo() {
     // this function queries firestore for the selected product document
 
     let docID = sessionStorage.getItem("docID"); // retrieve selected doc ID from session storage
     let productType = sessionStorage.getItem("productType")
 
     let docRef = db.collection(`products/${productType}/inventory`).doc(docID); // reference the product document
-    docRef.get().then(function (doc) {
+
+    try {
+        const doc = await docRef.get()
+    
         if (doc.exists) {
             populateProductDetails(doc);
         } else {
             // doc.data() will be undefined in this case
             console.log("No such document!");
         }
-    }).catch(function (error) {
+    } catch (error) {
         console.log("Error getting document:", error);
-    });
+    }
 }
 
 async function deleteProduct(docPath) {
