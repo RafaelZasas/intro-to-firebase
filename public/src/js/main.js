c421567e67870b7802bcad48b0a74226bee336c1
Rafael Zasas <zasas.rafi@gmail.com>
2021-02-13 01:15:35 -0800

    Refractor checkout.js to main.js

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -431,3 +431,48 @@ async function populateCart() {
     analytics.logEvent('view_cart', analyticsParams);
 
 }
+
+
+async function addShippingInfo(){
+    let billingSection = document.getElementById('billingInfo');
+    let shippingInfoSection = document.getElementById('shippingInfo');
+    shippingInfoSection.innerHTML += `
+    <hr class="solid">
+    `
+    document.getElementById('addShippingBtn').disabled = true;
+    billingSection.removeAttribute('hidden');
+
+}
+
+async function addBillingInfo(){
+    let billingSection = document.getElementById('billingInfo');
+    let cartInformationSection = document.getElementById('cartInformation');
+    let paymentSection = document.getElementById('purchaseSection');
+
+    let cartTotal = 0;
+    billingSection.innerHTML += `
+    <hr class="solid">
+    `
+    const snapshot = await getCart();
+    const cartItems = snapshot.docs.map(doc => ({id: doc.id, ...doc.data()}));
+
+    cartItems.forEach(item => {
+        cartTotal += item.price;
+        cartInformationSection.innerHTML += `
+            <div class="level columns-mobile">
+                <div class="level-left my-1 column-mobile">        
+                        <div class="mx-1"><p class="title is-5">${item.name}</p></div>  
+                        <div class="mx-2"><p class="title is-5">$${item.price}</p></div>   
+                </div>
+            </div>          
+        `;
+    })
+
+    cartInformationSection.innerHTML += `
+    <div class="column is-3 pl-0 pt-0"><hr class="solid"></div>
+    <h2 class="title is-3 has-text-left">Total: $${cartTotal}</h2>
+    <br>
+    `
+    document.getElementById('addBillingInfoBtn').disabled = true;
+    paymentSection.removeAttribute('hidden');
+}
