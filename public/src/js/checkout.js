cef3baecb2a3faa0f62f75f7c5d5d067c499d0e2
Rafael Zasas <zasas.rafi@gmail.com>
2021-02-13 01:00:51 -0800

    Add Payment Section

---
+++ public/src/js/checkout.js
@@ -0,0 +1,39 @@
+let shippingInfoSection = document.getElementById('shippingInfo');
+let billingSection = document.getElementById('billingInfo');
+let paymentSection = document.getElementById('purchaseSection');
+let cartInformationSection = document.getElementById('cartInformation');
+
+async function addShippingInfo(){
+    shippingInfoSection.innerHTML += `
+    <hr class="solid">
+    `
+    billingSection.removeAttribute('hidden');
+}
+
+async function addBillingInfo(){
+    billingSection.innerHTML += `
+    <hr class="solid">
+    `
+    const snapshot = await getCart();
+    const cartItems = snapshot.docs.map(doc => ({id: doc.id, ...doc.data()}));
+
+    cartItems.forEach(item => {
+
+        cartInformationSection.innerHTML += `
+            <div class="level columns-mobile">
+                <div class="level-left my-1 column-mobile">        
+                        <div class="mx-1"><p class="title is-5">${item.name}</p></div>  
+                        <div class="mx-2"><p class="title is-5">$${item.price}</p></div>   
+                </div>
+            </div>          
+        `;
+    })
+
+    cartInformationSection.innerHTML += `
+    <div class="column is-3 pl-0 pt-0"><hr class="solid"></div>
+    <h2 class="title is-3 has-text-left">Total: $150</h2>
+    <br>
+    `
+
+    paymentSection.removeAttribute('hidden');
+}
