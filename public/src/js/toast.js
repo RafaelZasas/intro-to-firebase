b0c3a428647f41947337bce73d2c252a66cd8238
ANDREYDEN <andrey2850@gmail.com>
2021-02-13 11:14:50 -0500

    Add dismiss time to toasts

--- public/src/js/toast.js
+++ public/src/js/toast.js
@@ -6,9 +6,10 @@
 function showToast(message, type) {
     const toastNode = document.createElement('div')
     toastNode.innerHTML = `
-    <div class="notification is-${type}" style="position: fixed; bottom: 1em; right: 1em;">
+    <div class="notification is-${type}" style="position: fixed; bottom: 1em; left: 1em;">
         <button class="delete" onclick="this.parentNode.remove()"></button>
         ${message}
     </div>`
     document.body.appendChild(toastNode)
+    setTimeout(() => toastNode.remove(), 5000)
 }
