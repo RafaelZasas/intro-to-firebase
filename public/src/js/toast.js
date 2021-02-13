242cbc447662494539a51b15bf1de2e4048acc43
ANDREYDEN <andrey2850@gmail.com>
2021-02-12 19:15:11 -0500

    Add close functionality for toasts

--- public/src/js/toast.js
+++ public/src/js/toast.js
@@ -2,7 +2,7 @@ function showToast(message, type) {
     const toastNode = document.createElement('div')
     toastNode.innerHTML = `
     <div class="notification is-${type}" style="position: fixed; bottom: 1em; right: 1em;">
-        <button class="delete"></button>
+        <button class="delete" onclick="this.parentNode.remove()"></button>
         ${message}
     </div>
     `
