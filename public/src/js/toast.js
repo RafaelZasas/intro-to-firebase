1cc9b48cf382e51f3727353c47a32b3eeb4225dc
ANDREYDEN <andrey2850@gmail.com>
2021-02-12 19:21:11 -0500

    Added docstrings

--- public/src/js/toast.js
+++ public/src/js/toast.js
@@ -1,10 +1,14 @@
+/**
+ * Displays a dismissable message in the bottom-right corner of the screen
+ * @param {string} message Message to display in the toast 
+ * @param {'primary'|'info'|'error'|'success'|'warning'|'danger'} type The type of toast to display
+ */
 function showToast(message, type) {
     const toastNode = document.createElement('div')
     toastNode.innerHTML = `
     <div class="notification is-${type}" style="position: fixed; bottom: 1em; right: 1em;">
         <button class="delete" onclick="this.parentNode.remove()"></button>
         ${message}
-    </div>
-    `
+    </div>`
     document.body.appendChild(toastNode)
 }
