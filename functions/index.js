b8462533e8a0251687880d06b726afbc3e3cd0ff
Rafael Zasas <zasas.rafi@gmail.com>
2021-02-19 22:17:33 -0800

    Implement promotion email for users with large cart

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,9 @@
 const functions = require("firebase-functions");
 const admin = require('firebase-admin');
-const { user } = require("firebase-functions/lib/providers/auth");
+const {user} = require("firebase-functions/lib/providers/auth");
 
 admin.initializeApp();
-db = admin.firestore();
+const db = admin.firestore();
 
 exports.updateInventorySize = functions.firestore
     .document('products/{categoryId}/inventory/{productId}')
@@ -52,12 +52,56 @@ exports.notifyOnPurchase = functions.firestore
             to: [userData.email],
             message: {
                 subject: 'Your Order Confirmation',
-                html: buildEmailHTML(purchaseData)
+                html: buildPurchaseConfirmationHTML(purchaseData)
             }
         })
     })
 
-function buildEmailHTML(purchaseData) {
+
+/**
+ * After a user has added items to their cart, send a coupon if:
+ * user hasn't received a promotion for this cart before
+ * AND if cart value > $500 .
+ */
+exports.sendPromotion = functions
+    .analytics
+    .event('add_to_cart')
+    .onLog(async (event) => {
+        const uid = event.user.userId; // The user ID set via the setUserId API.
+        const userData = await db.doc(`users/${uid}`).get(); // get users data from db
+        const snapshot = await db.collection(`users/${uid}/cart`).get();
+        // calculate the total of the cart
+        const cartTotal = snapshot.docs.reduce((acc, item) => acc + item.data().price, 0);
+
+        // For purchases above 500 USD, we send a coupon of 20% off the cart value.
+        if (userData.data()?.promotionSent && cartTotal > 500) {
+            // update user doc to specify that a promotional email has been sent to the user
+            await db.doc(`users/${uid}`).update({promotionSent: true});
+
+            // send an email to the user with the coupon
+            return db.collection('mail').add({
+                to: [userData.data().email],
+                message: {
+                    subject: 'We noticed you have quite a big cart',
+                    html: buildCouponHTML()
+                }
+            });
+        } else {
+            return null
+        }
+
+    });
+
+function buildCouponHTML() {
+    return `
+        <h1>We decided to offer you a coupon code for this purchase or the next</h1>
+
+        <h2>Use Coupon Code: <b><BIGSPENDER20/b> to get 20% off</h2>
+        <p>Thank you for being a valuable customer! <br> We hope this makes your experience <i>even better</i> </p>`
+}
+
+
+function buildPurchaseConfirmationHTML(purchaseData) {
     if (!purchaseData.items) {
         throw new Error('No items in the purchase data were provided')
     }
