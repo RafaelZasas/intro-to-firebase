6b137dd07edc5cf86484385973656580bd41b015
Clare So <clarmso@users.noreply.github.com>
2021-03-04 23:11:28 -0500

    Update public/src/js/profile.js
    Apply suggested change: Add curly braces for one-line if...else statement.
    
    Co-authored-by: Andrii Denysenko <andrey2850@gmail.com>

--- public/src/js/profile.js
+++ public/src/js/profile.js
@@ -15,8 +15,9 @@ async function populateProfile(user) {
 
     if (user) {
         document.getElementById('fullName').innerHTML = user.displayName;
-        if (userPhoto)
+        if (userPhoto) {
             document.getElementById('small_profile_image').setAttribute('src', userPhoto);
+        }
         document.getElementById('email-verified-success').style.display = user.emailVerified ? "inline-flex" : "none"
         document.getElementById('email-verified-failure').style.display = user.emailVerified ? "none" : "inline-flex"
     }
